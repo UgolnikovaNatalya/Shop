ce21cf81497c8ad3cdb4c17e3c4698d416b2636f
Ugolnikova Natalia <ugolok777@gmail.com>
2022-03-22 01:13:17 +0300

    Add adapter

--- app/src/main/java/com/example/shop/NearCaffee.kt
+++ app/src/main/java/com/example/shop/NearCaffee.kt
@@ -1,9 +1,10 @@
 package com.example.shop
 
 import android.os.Bundle
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.shop.data.MyAdapter
+import com.example.shop.data.testList
 import com.example.shop.databinding.ActivityNearCafeBinding
 
 class NearCafe: AppCompatActivity () {
@@ -11,14 +12,17 @@ class NearCafe: AppCompatActivity () {
         ActivityNearCafeBinding::bind,
         R.id.root
     )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_near_cafe)
 
-        viewBinding.btnMaps.setOnClickListener {
-            onStart()
+        val adapter = MyAdapter()
+        viewBinding.recyclerView.adapter = adapter
+        adapter.fillList(testList())
+
+        viewBinding.btnBack.setOnClickListener {
+            onBackPressed()
         }
-
-
     }
 }
